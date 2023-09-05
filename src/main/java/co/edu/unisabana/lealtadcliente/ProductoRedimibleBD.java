4de34ec1f807b117f45ddf660084d1b5c6340999
IvanGutierrez10 <ivangues@unisabana.edu.co>
2023-09-04 21:21:53 -0500

    Crear y programar tabla producto redimible y su repositorio
    Se creo la tabla correspondiente a los productos redimibles del ejercicio junto a su respectivo repositorio, esto mediante una nueva clase en java llamada "ProductoRedimibleBD" y una nueva interfaz llamada "ProductoRepository", con el fin de poder ser usada a futuro para añadir, eliminar y mdoificar clientes en la base de datos.

---
+++ src/main/java/co/edu/unisabana/lealtadcliente/ProductoRedimibleBD.java
@@ -0,0 +1,23 @@
+package co.edu.unisabana.lealtadcliente;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table (name = "productos_redimibles")
+public class ProductoRedimibleBD {
+    @Id
+    @Column
+    private int id;
+    @Column
+    private String nombre;
+    @Column
+    private String categoria;
+    @Column
+    private int valor;
+}
---
+++ src/main/java/co/edu/unisabana/lealtadcliente/ProductoRepository.java
@@ -0,0 +1,6 @@
+package co.edu.unisabana.lealtadcliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<ProductoRedimibleBD, Integer> {
+}
