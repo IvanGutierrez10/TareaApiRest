5d509bf8c1b95da7021bfc324b46cd74e902063d
Ivangutierrez10 <ivangues@unisabana.edu.co>
2023-09-08 22:29:22 -0500

    Crear validaciones de usuario
    Se creo un metodo para comprobar que el usuario ingresado si exista, esto con el fin de hacer las correspondientes validaciones y evitar problemas en ejecución.

--- src/main/java/co/edu/unisabana/lealtadcliente/controller/ClienteController.java
+++ src/main/java/co/edu/unisabana/lealtadcliente/controller/ClienteController.java
@@ -1,14 +1,11 @@
 package co.edu.unisabana.lealtadcliente.controller;
-import co.edu.unisabana.lealtadcliente.bd.ClienteBD;
-import co.edu.unisabana.lealtadcliente.bd.ClienteRepository;
+
 import co.edu.unisabana.lealtadcliente.controller.dto.ClienteDTO;
 import co.edu.unisabana.lealtadcliente.controller.dto.RespuestaDTO;
 import co.edu.unisabana.lealtadcliente.logica.LogicaCliente;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @RestController
@@ -18,12 +15,10 @@ public class ClienteController {
 
     @PutMapping(path="/cliente/actualizarpuntos/{cedula}/{nuevosPuntos}")
     public RespuestaDTO actualizarPuntos(@PathVariable int cedula, @PathVariable int nuevosPuntos){
-        this.logica.actualizarPuntos(cedula,nuevosPuntos);
-        return new RespuestaDTO("Puntos actualizados con exito");
+        return this.logica.actualizarPuntos(cedula,nuevosPuntos);
     }
     @PostMapping(path="/cliente/agregar")
     public RespuestaDTO agregarCliente(@RequestBody ClienteDTO clienteDTO){
-        this.logica.agregarUsuario(clienteDTO);
-        return new RespuestaDTO("Cliente agregado con éxito");
+        return this.logica.agregarUsuario(clienteDTO);
     }
 }
--- src/main/java/co/edu/unisabana/lealtadcliente/logica/LogicaCliente.java
+++ src/main/java/co/edu/unisabana/lealtadcliente/logica/LogicaCliente.java
@@ -3,10 +3,12 @@ package co.edu.unisabana.lealtadcliente.logica;
 import co.edu.unisabana.lealtadcliente.bd.ClienteBD;
 import co.edu.unisabana.lealtadcliente.bd.ClienteRepository;
 import co.edu.unisabana.lealtadcliente.controller.dto.ClienteDTO;
+import co.edu.unisabana.lealtadcliente.controller.dto.RespuestaDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,21 +16,37 @@ public class LogicaCliente {
 
     private ClienteRepository clienteRepository;
 
-    public void agregarUsuario(ClienteDTO clienteDTO){
-        ClienteBD clienteBD = new ClienteBD();
-        clienteBD.setCedula(clienteDTO.getCedula());
-        clienteBD.setNombre(clienteDTO.getNombre());
-        clienteBD.setApellido(clienteDTO.getApellido());
-        clienteBD.setPuntos(0);
-        clienteBD.setFecha_creacion(LocalDateTime.now());
-        clienteBD.setFecha_actualizacion(LocalDateTime.now());
-        this.clienteRepository.save(clienteBD);
+    public RespuestaDTO agregarUsuario(ClienteDTO clienteDTO){
+        if(clienteExiste(clienteDTO.getCedula())){
+            return new RespuestaDTO("Ya existe un usuario registrado con esa cedula");
+        }
+        else{
+            ClienteBD clienteBD = new ClienteBD();
+            clienteBD.setCedula(clienteDTO.getCedula());
+            clienteBD.setNombre(clienteDTO.getNombre());
+            clienteBD.setApellido(clienteDTO.getApellido());
+            clienteBD.setPuntos(0);
+            clienteBD.setFecha_creacion(LocalDateTime.now());
+            clienteBD.setFecha_actualizacion(LocalDateTime.now());
+            this.clienteRepository.save(clienteBD);
+            return new RespuestaDTO("Cliente creado con exito");
+        }
     }
 
-    public void actualizarPuntos(int cedula, int nuevosPuntos){
-        ClienteBD cliente = this.clienteRepository.getReferenceById(cedula);
-        cliente.setPuntos(nuevosPuntos);
-        cliente.setFecha_actualizacion(LocalDateTime.now());
-        this.clienteRepository.save(cliente);
+    public RespuestaDTO actualizarPuntos(int cedula, int nuevosPuntos){
+        if(clienteExiste(cedula)){
+            ClienteBD cliente = this.clienteRepository.getReferenceById(cedula);
+            cliente.setPuntos(nuevosPuntos);
+            cliente.setFecha_actualizacion(LocalDateTime.now());
+            this.clienteRepository.save(cliente);
+            return new RespuestaDTO("Los puntos del cliente han sido actualizados");
+        }
+        else {
+            return new RespuestaDTO("No existe ningun usuario registrado con esa cedula");
+        }
+    }
+
+    public boolean clienteExiste (int cedula){
+        return this.clienteRepository.existsById(cedula);
     }
 }
