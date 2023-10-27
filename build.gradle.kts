e12c1f492f757cb5e9b6dc2ce8130393b446cba1
Ivangutierrez10 <ivangues@unisabana.edu.co>
2023-10-27 15:36:31 -0500

    Configura basic authentication
    Se configuraron las dependencias de la seguridad del aplicativo y se creo un archivo para evitar que la seguridad genere error con los test

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.h2database:h2:2.2.220")
 	implementation("mysql:mysql-connector-java:8.0.32")
